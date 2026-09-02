9cd1e65d5c9603afb59cd0cd4979d94cb40940ba
Keerthana Muke <mukekeerthana@gmail.com>
2026-09-02 10:08:17 +0530

    Time: 68 ms (5.69%) | Memory: 19.7 MB (33.85%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote = sorted(ransomNote)
+        magazine = sorted(magazine)
+        i = j = 0
+        while i < len(ransomNote) and j < len(magazine):
+            if ransomNote[i] == magazine[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return i == len(ransomNote)
+
+        
